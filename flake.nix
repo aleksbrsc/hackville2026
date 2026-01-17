5135c42750697974b041f3ecac789605e5379fae
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-01-17 16:42:22 -0500

    Added nix flake, and pavlok python lib

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Python dev shell with pexpect";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # change if needed (e.g. aarch64-darwin)
+      pkgs = import nixpkgs { inherit system; };
+      python = pkgs.python3;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          python
+          python.pkgs.pexpect
+        ];
+      };
+    };
+}
+
